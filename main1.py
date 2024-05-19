984bf68902f43a4516d355744de1c5f7e508f9e0
Wildan77 <boyblanco77>
2024-05-19 21:09:13 +0700

    lanjut

--- main1.py
+++ main1.py
@@ -1,12 +1,16 @@
 import tugas9 as db
 
 while True :
-   print('selamat datang di sini ')
+   print('='*8,'selamat datang di sini ','='*8)
+   print('='*8,'Management stok barang ','='*8)
    print('pilihan')
    print('1. Tambah data barang ')
    print('2. hapus data barang ')
    print('3. tampilkan data barang ')
-   print('4. keluar')
+   print('4. edit data ')
+   print('5  Jumlah data ')
+   print('6  Cari data ')
+   print('7  keluar')
    pilihan = int(input('masukan plihan ')) 
    if pilihan == 1:
       db.insert()
@@ -15,5 +19,11 @@ while True :
    elif pilihan == 3:
       db.view_data()
    elif pilihan == 4:
+      db.edit_data()
+   elif pilihan == 5:
+      db.jumlah_data()
+   elif pilihan == 6:
+      db.cari_data()
+   elif pilihan == 7:
      print('terima kasih telah datang')
      break
